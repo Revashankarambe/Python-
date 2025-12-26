0dfddffd0b7458b9c0f2cb0908b962678fceb5d2
Revashankar Ambe <revashankarambe@gmail.com>
2025-12-26 22:47:58 +0530

    string slicling and string method

---
+++ day 8/strings slicling.py
@@ -0,0 +1,10 @@
+#strings slicing and operation on pythin
+name="rahul,ambe"
+print(name[0:5])
+print(len(name))
+fruit="mango"
+len1=len(fruit)
+print("mangon is a ",len1,"letter word.")
+nm="harry"
+print(nm[-4:-2])
+
---
+++ day 9/string methods.py
@@ -0,0 +1,15 @@
+a= "!!!!!!!rahul!!!!"
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("rahul","ambe"))
+blogheading= "introduction to my self"
+
+st1= "introduction to my self"
+print(len(st1.center(50))) 
+print(a.count("rahul"))
+
+str1= "welcome to the python !!!"
+print(str1.endswith("!!!"))
