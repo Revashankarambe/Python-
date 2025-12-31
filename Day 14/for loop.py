eade5a6fc7652dcb74297891889483ace272b1d1
Revashankar Ambe <revashankarambe@gmail.com>
2026-01-01 00:01:39 +0530

    Loops in Python

---
+++ Day 14/for loop.py
@@ -0,0 +1,19 @@
+# name = 'rahul'
+# for i in name:
+#     print(i,i)
+
+
+colors =["red","green","yellow","blue"]
+for color in colors:
+  print(color)
+  if(color=="red"):
+      print("this is a best color")
+  elif(color=="green"):
+      print("this is not best color ")
+  elif(color=="yellow"):
+     print("yellow is a fav")
+  elif(color=="blue"):
+     print("blue is cool color")
+  else:
+     print("unknown color")       
+
---
+++ Day 16/new year.py
@@ -0,0 +1,17 @@
+# New Year Countdown using while loop
+
+count = 10
+print("🎉 New Year Countdown Started 🎉")
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("🎆 HAPPY NEW YEAR 2026 🎆")
+
+# Celebration pattern using for loop
+
+for i in range(1, 6):
+    print("🎇 " * i)
+
+print("✨ Wishing you Success, Growth & Happiness ✨")
