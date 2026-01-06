f1d00fdaaf4d8bfc8ccb885fba97c63a423a5e04
Revashankar Ambe <revashankarambe@gmail.com>
2026-01-06 21:59:14 +0530

    introduction of list andlist methods

---
+++ Day 20/introduction to list.py
@@ -0,0 +1,14 @@
+# marks = [4,5,6]
+# if 5 in marks:
+#     print("yes")
+# else:
+#     print("no") 
+# same thing apply for string 
+# print(marks)  
+# print(marks[1:-1])
+# print(marks[1:2])
+
+lst = [i*i for i in range (10)]
+print(lst)
+lst =[i*i for i in range(10) if i%2==0]
+print(lst)
---
+++ Day 21/list methods.py
@@ -0,0 +1,7 @@
+l = [22,55,1,2,4,6]
+print(l)
+# l.append
+# l.sort()
+# l.reverse
+print(l.index(1))
+print(l)
