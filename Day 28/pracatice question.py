c47fa372e783f92a2094eea45b7941fe7f32933c
Revashankar Ambe <revashankarambe@gmail.com>
2026-01-20 23:32:53 +0530

    practice python

--- Day 28/pracatice question.py
+++ Day 28/pracatice question.py
@@ -1,15 +1,78 @@
-num = [ 10 ,20,30,40]
-print(num[3])
+#num = [ 10 ,20,30,40]
+#print(num[3])
 
-name = ["ram","shyam","mohan"]
-for i in range(len(name)):
-     print(name[i])
+#name = ["ram","shyam","mohan"]
+#for i in range(len(name)):
+     #print(name[i])
 
-num = [1 ,2 , 3 , 4 ,5]
-for i in range(len(num)):
-    print(num[i])
-num = [1,2 ,3 ,4,5,6,7,8,9]
-for n in num:          # loop start
-    if n % 2 == 0:     # even check
-        print(n)       # tabhi print
+#num = [1 ,2 , 3 , 4 ,5]
+#for i in range(len(num)):
+    #print(num[i])
+#num = [1,2 ,3 ,4,5,6,7,8,9]
+#for n in num:          # loop start
+    #if n % 2 == 0:     # even check
+        #print(n)       # tabhi print
 
+#num = int(input("Enter the value: "))
+
+#if num % 2 == 0:
+ #   print(num, "even number")
+#else:
+    #print(num, "odd number")
+#num = [5,2,9,1]
+#big = num[0]
+#for n in num:
+    #if n>big:
+        #big = n
+#print("this is big nuber",big) 
+
+num = [-1, -2, 3, -4, 5, -6]
+for n in num :
+ if n>=0:
+    print(n,"number is positive")
+else:
+    print(n,"number is negative")    
+
+num = []
+for i in range (6):
+   n= int(input("enter a number: "))
+   num.append(n)
+print("your list number",num) 
+
+
+question = [" what is your name"]
+option = ["1. rahul,2. shyam,3. rewa,4. ambe"]
+
+answer = [1]
+price = [5000]
+money = [5000]
+
+for i in range(len(question)):
+   print("\n",question[i])
+   for opt in option[i]:
+      print(opt)
+      ans = int(input("enter your anser:"))
+      if ans == answer[i]:
+         money=price[i]
+
+         print("correct answer",money)
+      else:
+         print("galat answer")
+         break
+      
+
+ 
+print("\n","game over")
+print("total money:",money)       
+
+         
+
+
+
+   
+
+   
+
+
+
+        
---
+++ Day 29/f string.py
@@ -0,0 +1,10 @@
+letter = "Hey my name is {}and I am from {}"
+name = "rahul"
+country = "india"
+print(f"Hey my name is {name} and I am from {country}")
+
+price = 4999.0999
+txt = f"for only {price:.2f} dollars!"
+print(txt)
+
+print(type(f"{2*56}"))
